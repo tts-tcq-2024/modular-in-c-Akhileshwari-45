4304417f55df520740589beeeccedcc48d1e8dbc
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-10-09 18:20:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
     test_pair_number_to_color(5, WHITE, SLATE);
     test_pair_number_to_color(21, VIOLET, BLUE);
 
- 
+    test_color_to_pair_number(YELLOW, BLUE, 16);
     test_color_to_pair_number(BLACK, ORANGE, 12);
     test_color_to_pair_number(VIOLET, SLATE, 25);
     test_reference_manual();
